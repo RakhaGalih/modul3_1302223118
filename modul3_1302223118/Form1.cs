09aa740753c1bcdf9145aee5158f7ea8dacc0239
PRAKTIKAN <PRAKTIKAN@DESKTOP-2G1A4E6>
2024-03-07 09:57:22 +0700

    Add project files.

---
+++ modul3_1302223118/Form1.cs
@@ -0,0 +1,30 @@
+namespace modul3_1302223118
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
